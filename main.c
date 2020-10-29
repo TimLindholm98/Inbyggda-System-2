7bab67f28e0e2ec3e5a48b834d2cbb58e5d9e669
TimLindholm98 <timpanmili98@gmail.com>
2020-10-29 12:20:29 +0100

    DONE - Deluppgift 3 (VG-krav): led-rampning

--- main.c
+++ main.c
@@ -10,37 +10,14 @@
 #include "serial.h"
 #include "timer.h"
 
-static volatile int indexing = 0;
-static volatile int pwm_value = 0;
-static volatile bool ramp_up = true;
 
 void main(void){
-	//printf_P(PSTR("Hello there\n"));
-	//uart_init();
 	timer_init();
 	LED_init();
-	//sei();
 	while(1){
-		
 		if(TIFR2 & (1<<OCF2A)){
 			OCR0A = simple_ramp();
 			TIFR2 |= ~(1<<OCF2A);
 		}
 	}
-}
-
-ISR(TIMER0_COMPA_vect){			//Ramping LED uncomment sei(); for interrupts
-	if(ramp_up){
-		if(pwm_value >= 255){
-			ramp_up = false;
-		}
-		pwm_value++;
-	}
-	else{
-		if(pwm_value <= 0){
-			ramp_up = true;
-		}
-		pwm_value--;
-	}
-	OCR0A = pwm_value;
 }
