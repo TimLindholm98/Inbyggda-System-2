f6f4d84af43834b327a7850805536ad1f0e94f2a
TimLindholm98 <timpanmili98@gmail.com>
2020-10-29 12:23:15 +0100

    DONE - Deluppgift 2: VERIERA LJUSSTYRKAN MED PWN

--- main.c
+++ main.c
@@ -10,14 +10,44 @@
 #include "serial.h"
 #include "timer.h"
 
+static volatile int pwm_value = 0;
+static volatile bool ramp_up = true;
 
 void main(void){
 	timer_init();
 	LED_init();
+	//sei();
 	while(1){
-		if(TIFR2 & (1<<OCF2A)){
-			OCR0A = simple_ramp();
-			TIFR2 |= ~(1<<OCF2A);
+		if(ramp_up){
+			pwm_value++;
+			if(pwm_value == 255){
+				ramp_up = false;
+			}
+		}
+		else{
+			pwm_value--;
+      if(pwm_value <= 0){
+				ramp_up = true;
+			}
+		}
+		_delay_ms(10);
+		OCR0A = pwm_value;
+	}
+}
+
+
+ISR(TIMER0_COMPA_vect){			//Ramping LED uncomment sei(); for interrupts
+	if(ramp_up){
+		pwm_value++;
+		if(pwm_value >= 255){
+			ramp_up = false;
 		}
 	}
+	else{
+		pwm_value--;
+		if(pwm_value <= 0){
+			ramp_up = true;
+		}
+	}
+	OCR0A = pwm_value;
 }
--- timer.c
+++ timer.c
@@ -5,35 +5,7 @@
 
 
 void timer_init() {
-  //TIMSK0 |= (1 << OCIE0A); // sätter på interrupts för  TIMER0_COMPA
-  TCCR0A |= (1 << WGM01) | (1 << WGM00) | (1 << COM0A1);    // Fast PWM-mode
-  TCCR0B |= (1 << CS00) | (1 << CS01);  //Set prescaler
-
-  OCR2A = 249; //COMPARE_MATCH_REGISTER
-  //TIMSK0 = (1 << OCIE0A);
-  TCCR2A |= (1 << WGM21); // enable ctc mode
-  TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);  //Set prescaler
-
-  /*TIMSK0 |= (1 << OCIE0A); // sätter på interrupts för  TIMER0_COMPA
-  TCCR0A |= (1 << WGM01) | (1 << WGM00) | (1 << COM0A1);
-  TCCR0B |= (1 << CS00) | (1 << CS01) /*| (1 << WGM02)*/;  //Set prescaler*/
-}
-
-uint8_t simple_ramp(){
-  static bool ramp_up = true;
-  static int pwm_value = 0;
-
-  if(ramp_up){
-    pwm_value++;
-    if(pwm_value >= 255){
-			ramp_up = false;
-		}
-	}
-	else{
-    pwm_value--;
-		if(pwm_value <= 0){
-			ramp_up = true;
-		}
-	}
-  return pwm_value;
+   TIMSK0 |= (1 << OCIE0A); // sätter på interrupts för  TIMER0_COMPA
+   TCCR0A |= (1 << WGM01) | (1 << WGM00) | (1 << COM0A1);
+   TCCR0B |= (1 << CS00) | (1 << CS01);  //Set prescaler
 }
